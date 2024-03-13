4121f6ee06c1e6a7d246f97777e942fc517baff4
Matthias Diener <mdiener@illinois.edu>
2024-03-13 16:43:54 -0500

    move to setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 def main():
     setup(name="qdset",
-          version="1.0.0",
+          version="2024.0",
           description="qdset",
-          author="<your name>",
-          author_email="your_email@gmail.com",
-          ext_modules=[Extension("qdset", ["setobject.c"])])
+          author="Matthias Diener",
+          author_email="mdiener@illinois.edu",
+          ext_modules=[Extension("qdset", ["setobject.c"],
+                                 extra_compile_args=["-g"],
+                                 depends=["setobject.h"])
+                       ],
+          )
 
 if __name__ == "__main__":
     main()
